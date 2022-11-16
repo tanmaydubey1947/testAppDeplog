00a5fe09de53637d9ea43a4ab46dbd3ea4879507
TANMAY DUBEY <48459376+tanmaydubey1947@users.noreply.github.com>
2022-11-16 13:51:47 +0530

    Added Post Mapping test class.

--- deployApp/src/main/java/com/test/deployApp/Controller.java
+++ deployApp/src/main/java/com/test/deployApp/Controller.java
@@ -1,8 +1,12 @@
 package com.test.deployApp;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.test.deployApp.vo.TestVOClass;
 
 @RestController
 @RequestMapping(path = "/testApp")
@@ -12,5 +16,16 @@ public class Controller {
 	@GetMapping(path = "/testMethod", produces = "application/json")
 	public String testMethod() {
 		return "Application is running.";
+	}
+	
+	@PostMapping(path = "/testPostMapping", produces = "application/json")
+	public TestVOClass postMappingMethod(@RequestBody TestVOClass testVOInput) {
+		
+		TestVOClass testVOClass = new TestVOClass();
+		
+		testVOClass.setName(testVOInput.getName());
+		testVOClass.setAge(testVOInput.getAge());
+		
+		return testVOClass;
 	}
 }
---
+++ deployApp/src/main/java/com/test/deployApp/vo/TestVOClass.java
@@ -0,0 +1,35 @@
+package com.test.deployApp.vo;
+
+public class TestVOClass {
+
+	String name;
+	int age;
+
+	public TestVOClass() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public TestVOClass(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
