071d685c3fe2a7d55b94023281eb8dc261098834
TANMAY DUBEY <48459376+tanmaydubey1947@users.noreply.github.com>
2022-11-16 13:26:41 +0530

    Intial Commit

---
+++ deployApp/src/main/java/com/test/deployApp/TestDeployAppApplication.java
@@ -0,0 +1,13 @@
+package com.test.deployApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDeployAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDeployAppApplication.class, args);
+	}
+
+}
---
+++ deployApp/src/test/java/com/test/deployApp/TestDeployAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.deployApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDeployAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
